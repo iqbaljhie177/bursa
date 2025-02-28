9f6a88523b6bcf52abd82f7565ae906db0734c4a
Iqbal Baharuddin <76838731+iqbaljhie177@users.noreply.github.com>
2025-02-28 13:23:56 -0800

    Update App.tsx

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ChangePassword from "./pages/ChangePassword";
-import DepositRequest from "./pages/DepositReq";
-import WithdrawRequest from "./pages/WithdrawReq";
-import KYCRequest from "./pages/KycReq";
-import PaymentMethod from "./pages/PaymentMethod";
-import ForgotPassword from "./pages/Profile";
-import Trading from "./pages/Trading";
+import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
+import ChangePassword from "./pages/ChangePassword.tsx";
+import DepositRequest from "./pages/DepositReq.tsx";
+import WithdrawRequest from "./pages/WithdrawReq.tsx";
+import KYCRequest from "./pages/KycReq.tsx";
+import PaymentMethod from "./pages/PaymentMethod.tsx";
+import ForgotPassword from "./pages/Profile.tsx";
+import Trading from "./pages/Trading.tsx";
 
 const App = () => {
   return (
